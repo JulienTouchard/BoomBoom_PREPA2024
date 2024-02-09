085034a35c61529ed291d01b9c4aa17351a6dbf9
JulienTouchard <julien.touchard@free.fr>
2024-02-09 12:22:43 +0100

    match and menu

--- login.php
+++ login.php
@@ -8,23 +8,22 @@ $error = "";
 // est ce qu'un utilisateur a cliquer sur le submit?
 if (isset($_POST["submited"])) {
     $email = $_POST['email'];
-    $password = $_POST['password'];//mot de pass en clair
+    $password = $_POST['password']; //mot de pass en clair
     // me permet de tester si l'email envoyer dans mon formulaire
     // existe dans ma base de données
     $rq = "SELECT * FROM `user` WHERE email = '$email'";
-    
+
     $results = $pdo->prepare($rq);
     $results->execute();
     $results = $results->fetch();
-    if($results){
+    if ($results) {
         //password_verify($password, $passwordVerif) return boolean
-        $passwordVerif = $results['password'];//mot de passe hashé qui vient de ma bdd
-        if(password_verify($password,$passwordVerif)){
+        $passwordVerif = $results['password']; //mot de passe hashé qui vient de ma bdd
+        if (password_verify($password, $passwordVerif)) {
             header("Location: match.php?email=$email");
         } else {
             $error = "Password incorrects...";
         }
-        
     } else {
         $error = "Identifiants incorrects...";
     }
@@ -43,17 +42,20 @@ if (isset($_POST["submited"])) {
 </head>
 
 <body>
-    Hello world
+    <header>
+        <?php include("./inc/menu.php"); ?>
+    </header>
+
     <?php
     /* Ici je déclare une variable avec le signe $ attaché */
     $unePhrase = "Hello world again!";
     // echo() est une fonction qui permet d'envoyer des informations 
     // au client
-    echo ($unePhrase);
+    // echo ($unePhrase);
     // var_dump() est l'equivalent de la fonction javascript console.dir()
     // mais elle ne peut pas utiliser la console et affiche le resutat 
     // dans la page web
-    var_dump($unePhrase);
+    // var_dump($unePhrase);
 
     ?>
     <h1>Identifiez-vous</h1>
@@ -70,7 +72,7 @@ if (isset($_POST["submited"])) {
         </div>
     </form>
     <div>
-        <?php echo($error); ?>
+        <?php echo ($error); ?>
     </div>
 </body>
 
